bf2da8249cbe315b280f19748d28819d252befc0
Manish Tamang <126256480+Manishtmang@users.noreply.github.com>
2024-01-07 16:02:54 +0000

    update

--- app/(website)/about/about.js
+++ app/(website)/about/about.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function About({ authors, settings }) {
   return (
     <Container>
-      <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight lg:text-4xl lg:leading-snug dark:text-white">
+      <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
         About
       </h1>
       <div className="text-center">
@@ -14,15 +14,26 @@ export default function About({ authors, settings }) {
       </div>
 
       <div className="mb-16 mt-6 grid grid-cols-3 gap-5 md:mb-32 md:mt-16 md:gap-16">
-        <div className="relative aspect-square overflow-hidden rounded-md odd:translate-y-10 odd:md:translate-y-16">
-          {/* <Image
-            src={}
-            alt={"Manish"}
-            fill
-            sizes="(max-width: 320px) 100vw, 320px"
-            className="object-cover"
-          /> */}
-        </div>
+        {authors.slice(0, 3).map(author => {
+          const imageProps = urlForImage(author?.image) || null;
+          return (
+            <div
+              key={author._id}
+              className="relative aspect-square overflow-hidden rounded-md bg-slate-50 odd:translate-y-10 odd:md:translate-y-16">
+              <Link href={`/author/${author?.slug}`}>
+                {imageProps && (
+                  <Image
+                    src={imageProps?.src}
+                    alt={author?.name || " "}
+                    fill
+                    sizes="(max-width: 320px) 100vw, 320px"
+                    className="object-cover"
+                  />
+                )}
+              </Link>
+            </div>
+          );
+        })}
       </div>
 
       <div className="prose mx-auto mt-14 text-center dark:prose-invert">
