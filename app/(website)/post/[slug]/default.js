6b9536bab48295f0c094324a552d7e7d805d23ed
Manish Tamang <126256480+Manishtmang@users.noreply.github.com>
2024-01-07 16:00:50 +0000

    up

--- app/(website)/post/[slug]/default.js
+++ app/(website)/post/[slug]/default.js
@@ -5,38 +5,24 @@ import { notFound } from "next/navigation";
 import { PortableText } from "@/lib/sanity/plugins/portabletext";
 import { urlForImage } from "@/lib/sanity/image";
 import { parseISO, format } from "date-fns";
-import { fetcher } from "@/lib/sanity/client";
-import {useSWR} from "swr";
-import { paginatedquery } from "@/lib/sanity/groq";
 
 import CategoryLabel from "@/components/blog/category";
 import AuthorCard from "@/components/blog/authorCard";
 
-export default function Post() {
-  const [isLoading, setIsLoading] = useState(false);
-  const {
-    data: post,
-    error,
-    isValidating
-  } = useSWR([paginatedquery], fetcher, {
-    fallbackData: [],
-    onSuccess: () => {
-      setIsLoading(false); // You need to define setIsLoading somewhere
-    }
-  });
-
+export default function Post(props) {
+  const { loading, post } = props;
 
   const slug = post?.slug;
 
-  if (!isLoading && !slug) {
+  if (!loading && !slug) {
     notFound();
   }
 
   const imageProps = post?.mainImage
-    ? urlForImage(post.mainImage)
+    ? urlForImage(post?.mainImage)
     : null;
 
-  const authorImageProps = post?.author?.image
+  const AuthorimageProps = post?.author?.image
     ? urlForImage(post.author.image)
     : null;
 
@@ -48,17 +34,17 @@ export default function Post() {
             <CategoryLabel categories={post.categories} />
           </div>
 
-          <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight lg:text-4xl lg:leading-snug dark:text-white">
+          <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
             {post.title}
           </h1>
 
-          <div className="mt-3 flex justify-center space-x-3 text-gray-500">
+          <div className="mt-3 flex justify-center space-x-3 text-gray-500 ">
             <div className="flex items-center gap-3">
               <div className="relative h-10 w-10 flex-shrink-0">
-                {authorImageProps && (
+                {AuthorimageProps && (
                   <Link href={`/author/${post.author.slug.current}`}>
                     <Image
-                      src={authorImageProps.src}
+                      src={AuthorimageProps.src}
                       alt={post?.author?.name}
                       className="rounded-full object-cover"
                       fill
@@ -76,8 +62,7 @@ export default function Post() {
                 <div className="flex items-center space-x-2 text-sm">
                   <time
                     className="text-gray-500 dark:text-gray-400"
-                    dateTime={post?.publishedAt || post._createdAt}
-                  >
+                    dateTime={post?.publishedAt || post._createdAt}>
                     {format(
                       parseISO(post?.publishedAt || post._createdAt),
                       "MMMM dd, yyyy"
@@ -96,7 +81,6 @@ export default function Post() {
           <Image
             src={imageProps.src}
             alt={post.mainImage?.alt || "Thumbnail"}
-            priority={preloadImage} // Assuming preloadImage is defined somewhere
             loading="eager"
             fill
             sizes="100vw"
@@ -104,6 +88,7 @@ export default function Post() {
           />
         )}
       </div>
+
       <Container>
         <article className="mx-auto max-w-screen-md ">
           <div className="prose mx-auto my-3 dark:prose-invert prose-a:text-blue-600">
@@ -112,8 +97,7 @@ export default function Post() {
           <div className="mb-7 mt-7 flex justify-center">
             <Link
               href="/"
-              className="bg-brand-secondary/20 rounded-full px-5 py-2 text-sm text-blue-600 dark:text-blue-500"
-            >
+              className="bg-brand-secondary/20 rounded-full px-5 py-2 text-sm text-blue-600 dark:text-blue-500 ">
               ← View all posts
             </Link>
           </div>
