97884a047702889a201caaf872d0125b1af6f72f
Manish-Tamang <maneshtamang833@gmail.com>
2025-01-18 14:20:26 +0545

    Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Menu, Transition, Disclosure } from "@headlessui/react";
 import Container from "@/components/container";
 import Link from "next/link";
@@ -8,40 +8,42 @@ import Image from "next/image";
 import { urlForImage } from "@/lib/sanity/image";
 import cx from "clsx";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
-import { myLoader } from "@/utils/all";
 
 export default function Navbar(props) {
   const leftmenu = [
     {
       label: "Home",
-      href: "/"
+      href: "/",
     },
-    // {
-    //   label: "About",
-    //   href: "/about"
-    // },
+   {
+      label: "About",
+      href: "/about",
+    },
     {
-    label: "Contact",
-    href: "/contact"
-  }
+      label: "Contact",
+      href: "/contact",
+    },
   ];
 
   const rightmenu = [
     {
       label: "Archive",
-      href: "/archive"
+      href: "/archive",
     },
-    // {
-    //   label: "Pro Version",
-    //   href: "https://stablo-pro.web3templates.com/",
-    //   external: true,
-    //   badge: "new"
-    // },
-    // {
-    //   label: "Download",
-    //   href: "https://web3templates.com/templates/stablo-minimal-blog-website-template",
-    //   external: true
-    // }
+    {
+      label: "More",
+      children: [
+       
+          {
+            label: "Github",
+            href: "https://github.com/Manish-Tamang",
+            external: true,
+          },
+        
+         
+       
+      ],
+    },
   ];
 
   const mobilemenu = [...leftmenu, ...rightmenu];
@@ -66,9 +68,10 @@ export default function Navbar(props) {
                         <Link
                           href={item.href}
                           key={`${item.label}${index}`}
-                          className="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
+                          className="px-4 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
                           target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}>
+                          rel={item.external ? "noopener" : ""}
+                        >
                           {item.label}
                         </Link>
                       )}
@@ -77,46 +80,20 @@ export default function Navbar(props) {
                 </div>
                 <div className="flex w-full items-center justify-between md:w-auto">
                   <Link href="/" className="w-28 dark:hidden">
-                    {/* {props.logo ? (
-                      <Image
-                        {...urlForImage(props.logo)}
-                        alt="Logo"
-                        priority={true}
-                        sizes="(max-width: 640px) 100vw, 200px"
-                      />
-                    ) : (
-                      <span className="block text-center">
-                        Manish
-                      </span>
-                    )} */}
-                    <span className="block text-center">
-                        Manish
-                      </span>
+                    <span className="block text-center">Manish</span>
                   </Link>
                   <Link href="/" className="hidden w-28 dark:block">
-                    {/* {props.logoalt ? (
-                      <Image
-                        {...urlForImage(props.logoalt)}
-                        alt="Logo"
-                        priority={true}
-                        sizes="(max-width: 640px) 100vw, 200px"
-                      />
-                    ) : (
-                      <span className="block text-center">
-                        Manish
-                      </span>
-                    )} */}
-                    <span className="block text-center">
-                        Manish
-                      </span>
+                    <span className="block text-center">Manish</span>
                   </Link>
                   <Disclosure.Button
                     aria-label="Toggle Menu"
-                    className="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 md:hidden ">
+                    className="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 md:hidden "
+                  >
                     <svg
                       className="h-6 w-6 fill-current"
                       xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24">
+                      viewBox="0 0 24 24"
+                    >
                       {open && (
                         <path
                           fillRule="evenodd"
@@ -147,15 +124,11 @@ export default function Navbar(props) {
                         <Link
                           href={item.href}
                           key={`${item.label}${index}`}
-                          className="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
+                          className="px-4 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
                           target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}>
+                          rel={item.external ? "noopener" : ""}
+                        >
                           <span> {item.label}</span>
-                          {item.badge && (
-                            <span className="ml-2 rounded bg-blue-100 px-2 py-0.5 text-xs font-semibold text-blue-600 dark:bg-cyan-200 dark:text-blue-800 ">
-                              {item.badge}
-                            </span>
-                          )}
                         </Link>
                       )}
                     </Fragment>
@@ -177,9 +150,10 @@ export default function Navbar(props) {
                         <Link
                           href={item.href}
                           key={`${item.label}${index}`}
-                          className="w-full px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
+                          className="w-full px-4 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
                           target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}>
+                          rel={item.external ? "noopener" : ""}
+                        >
                           {item.label}
                         </Link>
                       )}
@@ -196,58 +170,72 @@ export default function Navbar(props) {
 }
 
 const DropdownMenu = ({ menu, items, mobile }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true)
+  }
+    const handleClose = () => {
+    setIsOpen(false)
+  }
+
   return (
     <Menu
       as="div"
-      className={cx("relative text-left", mobile && "w-full")}>
-      {({ open }) => (
-        <>
-          <Menu.Button
-            className={cx(
-              "flex items-center gap-x-1 rounded-md px-5 py-2 text-sm font-medium  outline-none transition-all focus:outline-none focus-visible:text-indigo-500 focus-visible:ring-1 dark:focus-visible:bg-gray-800",
-              open
-                ? "text-blue-500 hover:text-blue-500"
-                : " text-gray-600 dark:text-gray-400 ",
-              mobile ? "w-full px-4 py-2 " : "inline-block px-4 py-2"
-            )}>
-            <span>{menu.label}</span>
-            <ChevronDownIcon className="mt-0.5 h-4 w-4" />
-          </Menu.Button>
-          <Transition
-            as={Fragment}
-            enter="lg:transition lg:ease-out lg:duration-100"
-            enterFrom="lg:transform lg:opacity-0 lg:scale-95"
-            enterTo="lg:transform lg:opacity-100 lg:scale-100"
-            leave="lg:transition lg:ease-in lg:duration-75"
-            leaveFrom="lg:transform lg:opacity-100 lg:scale-100"
-            leaveTo="lg:transform lg:opacity-0 lg:scale-95">
-            <Menu.Items
-              className={cx(
-                "z-20 origin-top-left rounded-md  focus:outline-none  lg:absolute lg:left-0  lg:w-56",
-                !mobile && "bg-white shadow-lg  dark:bg-gray-800"
-              )}>
-              <div className={cx(!mobile && "py-3")}>
-                {items.map((item, index) => (
-                  <Menu.Item as="div" key={`${item.title}${index}`}>
-                    {({ active }) => (
-                      <Link
-                        href={item?.path ? item.path : "#"}
-                        className={cx(
-                          "flex items-center space-x-2 px-5 py-2 text-sm lg:space-x-4",
-                          active
-                            ? "text-blue-500"
-                            : "text-gray-700 hover:text-blue-500 focus:text-blue-500 dark:text-gray-300"
-                        )}>
-                        <span> {item.title}</span>
-                      </Link>
+      className={cx("relative text-left", mobile && "w-full")}
+      onMouseLeave={handleClose}
+      onMouseEnter={handleOpen}
+    >
+       <Menu.Button
+          className={cx(
+            "flex items-center gap-x-1 rounded-md px-4 py-2 text-sm font-medium  outline-none transition-all focus:outline-none focus-visible:text-indigo-500 focus-visible:ring-1 dark:focus-visible:bg-gray-800",
+            isOpen
+              ? "text-blue-500 hover:text-blue-500"
+              : " text-gray-600 dark:text-gray-400 ",
+            mobile ? "w-full px-4 py-2 " : "inline-block px-4 py-2"
+          )}
+        >
+        <span>{menu.label}</span>
+        <ChevronDownIcon className="mt-0.5 h-4 w-4" />
+        </Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="lg:transition lg:ease-out lg:duration-100"
+        enterFrom="lg:transform lg:opacity-0 lg:scale-95"
+        enterTo="lg:transform lg:opacity-100 lg:scale-100"
+        leave="lg:transition lg:ease-in lg:duration-75"
+        leaveFrom="lg:transform lg:opacity-100 lg:scale-100"
+        leaveTo="lg:transform lg:opacity-0 lg:scale-95"
+      >
+        <Menu.Items
+          className={cx(
+            "z-20 origin-top-left rounded-md  focus:outline-none lg:absolute lg:left-0  lg:w-56",
+            !mobile && "bg-white shadow-lg  dark:bg-gray-800"
+          )}
+        >
+          <div className={cx(!mobile && "py-3")}>
+            {items.map((item, index) => (
+              <Menu.Item as="div" key={`${item.label}${index}`}>
+                {({ active }) => (
+                  <Link
+                    href={item?.href ? item.href : "#"}
+                    className={cx(
+                      "flex items-center space-x-2 px-5 py-2 text-sm lg:space-x-4",
+                      active
+                        ? "text-blue-500"
+                        : "text-gray-700 hover:text-blue-500 focus:text-blue-500 dark:text-gray-300"
                     )}
-                  </Menu.Item>
-                ))}
-              </div>
-            </Menu.Items>
-          </Transition>
-        </>
-      )}
+                    target={item.external ? "_blank" : ""}
+                    rel={item.external ? "noopener" : ""}
+                  >
+                    <span> {item.label}</span>
+                  </Link>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
+      </Transition>
     </Menu>
   );
 };
