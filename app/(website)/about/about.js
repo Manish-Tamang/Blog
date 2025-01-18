f59e7aa3646035b84d4c4b142d55498b1bfaa1e4
Manish-Tamang <maneshtamang833@gmail.com>
2025-01-18 16:08:15 +0545

    Final Touch

--- app/(website)/about/about.js
+++ app/(website)/about/about.js
@@ -1,57 +1,289 @@
+/* eslint-disable */
+
+"use client";
 import Container from "@/components/container";
 import { urlForImage } from "@/lib/sanity/image";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  FaReact,
+  FaNodeJs,
+  FaGithub,
+  FaNpm,
+  FaPython,
+  FaDocker
+} from "react-icons/fa";
+import {
+  SiNextdotjs,
+  SiTailwindcss,
+  SiSanity,
+  SiTypescript
+} from "react-icons/si";
+import { TbBrandVercel } from "react-icons/tb";
+import { useState, useEffect } from "react";
 
 export default function About({ authors, settings }) {
+  const [stars, setStars] = useState(0);
+  useEffect(() => {
+    const fetchStars = async () => {
+      try {
+        const response = await fetch(
+          "https://api.github.com/repos/Manish-Tamang/Blog"
+        );
+        const data = await response.json();
+        setStars(data.stargazers_count || 0);
+      } catch (error) {
+        console.error("Failed to fetch repo stars", error);
+      }
+    };
+    fetchStars();
+  }, []);
   return (
     <Container>
-      <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
-        About
+      <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight lg:text-4xl lg:leading-snug dark:text-white">
+        About This Blog
       </h1>
-      <div className="text-center">
-        <p className="text-lg">We are a small passionate team.</p>
+      <div className="mb-8 text-center">
+        <p className="text-lg">
+          This blog is a personal project created by{" "}
+          <span className="text-brand-primary font-medium dark:text-blue-400">
+            Manish Tamang
+          </span>
+          . It was conceived during my early days of learning Next.js,
+          at a time when the shift from the pages router to the app
+          router had just taken place. I initially started with Stablo
+          templates, and then embarked on the journey to customise and
+          make it my own.
+        </p>
+        <p className="mt-2 text-lg">
+          This project served as a perfect practical learning
+          experience. Through reverse engineering and meticulous code
+          dissection, I aimed to solidify my understanding of
+          Next.js's app router concepts. It is worth noting that all
+          of the authors you see featured in this blog are actually my
+          friends.
+        </p>
+        <p className="mt-2 text-lg">
+          Here's a glimpse into the technologies used to build this
+          blog:
+        </p>
       </div>
 
-      <div className="mb-16 mt-6 grid grid-cols-3 gap-5 md:mb-32 md:mt-16 md:gap-16">
-        {authors.slice(0, 3).map(author => {
-          const imageProps = urlForImage(author?.image) || null;
-          return (
-            <div
-              key={author._id}
-              className="relative aspect-square overflow-hidden rounded-md bg-slate-50 odd:translate-y-10 odd:md:translate-y-16">
-              <Link href={`/author/${author?.slug}`}>
-                {imageProps && (
-                  <Image
-                    src={imageProps?.src}
-                    alt={author?.name || " "}
-                    fill
-                    sizes="(max-width: 320px) 100vw, 320px"
-                    className="object-cover"
-                  />
-                )}
-              </Link>
-            </div>
-          );
-        })}
+      <h2 className="text-brand-primary mb-3 mt-8 text-center text-2xl font-semibold tracking-tight lg:text-3xl lg:leading-snug dark:text-white">
+        Tech Stack
+      </h2>
+
+      <div className="mt-8 grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4">
+        <TechCard
+          name="Next.js"
+          version="13.4.12"
+          color="#000000"
+          Icon={SiNextdotjs}
+        />
+        <TechCard
+          name="React"
+          version="18.2.0"
+          color="#61dafb"
+          Icon={FaReact}
+        />
+        <TechCard
+          name="Tailwind CSS"
+          version="3.3.1"
+          color="#06B6D4"
+          Icon={SiTailwindcss}
+        />
+        <TechCard
+          name="Sanity"
+          version="3.9.0"
+          color="#f03e2f"
+          Icon={SiSanity}
+        />
+        <TechCard
+          name="Node.js"
+          version="v18.16.0"
+          color="#68a063"
+          Icon={FaNodeJs}
+        />
+        <TechCard
+          name="Typescript"
+          version="5.0.4"
+          color="#3178c6"
+          Icon={SiTypescript}
+        />
+        <TechCard name="Github" Icon={FaGithub} color="#171515" />
+        <TechCard
+          name="Vercel"
+          Icon={TbBrandVercel}
+          color="#000000"
+        />
+        <TechCard name="npm" Icon={FaNpm} color="#CB3837" />
       </div>
 
-      <div className="prose mx-auto mt-14 text-center dark:prose-invert">
-        <p>
-          We provide real-time connectivity to enable software
-          providers and financial institutions to build integrated
-          products for their small business customers.
-        </p>
-        <p>
-          Our API infrastructure is leveraged by clients ranging from
-          lenders to corporate card providers and business forecasting
-          tools, with use cases including automatic reconciliation,
-          business dashboarding, and loan decisioning.
-        </p>
-        <p>
-          <Link href="/contact">Get in touch</Link>
-        </p>
+      <h2 className="text-brand-primary mb-3 mt-8 text-center text-2xl font-semibold tracking-tight lg:text-3xl lg:leading-snug dark:text-white">
+        Project Structure
+      </h2>
+      <p className="mb-8 mt-2 text-center text-lg">
+        Here is the project folder structure:
+      </p>
+
+      <pre className="overflow-x-auto rounded-lg bg-gray-100 px-5 py-5 text-sm dark:bg-gray-800">
+        {`├── .eslintignore        # Files to be ignored by ESLint.
+├── .eslintrc          # Configuration for ESLint.
+├── .gitignore         # Files to be ignored by Git.
+├── .prettierrc        # Configuration for Prettier code formatting.
+├── .vscode           # Configuration directory for VSCode.
+│   └── settings.json    # VSCode settings.
+├── README.md          # Project README.
+├── app                 # Next.js application directory (App Router).
+│   ├── (sanity)       # Routes related to Sanity Studio.
+│   │   ├── layout.tsx    # Layout for Sanity Studio.
+│   │   └── studio      # Sanity Studio routes.
+│   │       └── [[...index]]
+│   │           ├── head.tsx     # Head component for Sanity Studio.
+│   │           └── page.tsx     # Sanity Studio page.
+│   ├── (website)      # Routes related to the main website.
+│   │   ├── about      # About page related files.
+│   │   │   ├── about.js  # About page component.
+│   │   │   └── page.js   # About page data fetching logic.
+│   │   ├── archive    # Archive page related files.
+│   │   │   ├── archive.js # Archive page component.
+│   │   │   └── page.js  # Archive page data fetching logic.
+│   │   ├── author     # Author related files.
+│   │   │   └── [slug]  # Dynamic author route.
+│   │   │       ├── default.js # Author page default component.
+│   │   │       └── page.js  # Author page data fetching logic.
+│   │   ├── contact    # Contact page related files.
+│   │   │   ├── contact.js # Contact page component.
+│   │   │   └── page.js  # Contact page data fetching logic.
+│   │   ├── home.js     # Home page component.
+│   │   ├── layout.tsx  # Root layout for website pages.
+│   │   ├── page.js     # Home page data fetching logic.
+│   │   └── post       # Post related files.
+│   │       └── [slug]  # Dynamic post route.
+│   │           ├── default.js # Post page default component.
+│   │           ├── opengraph-todo.js  # Open Graph image generator.
+│   │           └── page.js  # Post page data fetching logic.
+│   ├── api          # API routes.
+│   │   └── hello      # Example API route.
+│   │       └── route.ts  # API route handler.
+│   ├── favicon.ico     # Favicon file
+│   ├── layout.tsx    # Root layout of the app.
+│   └── providers.jsx  # Theme provider
+├── components        # Reusable React components.
+│   ├── blog           # Blog related components.
+│   │   ├── authorCard.js  # Author card component.
+│   │   └── category.js    # Category label component.
+│   ├── container.js   # Container component for page layouts.
+│   ├── featured.js    # Featured post component.
+│   ├── footer.js      # Footer component.
+│   ├── layout.js      # Layout component (legacy).
+│   ├── navbar.js     # Main navbar component
+│   ├── navbaralt.js  # Alternate navbar component (legacy)
+│   ├── ogimage.js     # Open Graph image component.
+│   ├── postalt.js     # Alternate post component.
+│   ├── postlist.js    # Post list item component.
+│   ├── sidebar.js     # Sidebar component.
+│   ├── themeSwitch.js  # Theme switch component.
+│   └── ui          # UI related components.
+│       ├── label.js      # Label component.
+│       ├── search.js    # Search input component.
+│       └── time.js       # Time display component.
+├── lib             # Library files.
+│   └── sanity       # Sanity.io related files.
+│       ├── client.ts   # Sanity client setup and data fetching.
+│       ├── config.ts   # Sanity configuration.
+│       ├── groq.js     # GROQ queries.
+│       ├── image.js     # Image helper functions.
+│       ├── plugins    # Sanity Studio plugins.
+│       │   ├── portabletext.js   # Portable Text serialization setup.
+│       │   └── previews     # Sanity Studio previews.
+│       │       ├── BlogPreview.js
+│       │       ├── BlogPreview.jsx
+│       │       ├── IframePreview.css
+│       │       ├── PagePreview.js
+│       │       └── PagePreview.jsx
+│       └── settings.tsx  # Sanity Singleton setup plugin
+│       └── schemas       # Sanity schema definitions.
+│           ├── author.js   # Author schema.
+│           ├── blockContent.js # Block content schema.
+│           ├── category.js # Category schema.
+│           ├── index.ts    # Entry point for schemas.
+│           ├── post.js     # Post schema.
+│           ├── previews #  Preview components.
+│           │    ├── iframe.jsx # iFrame Preview component
+│           │    └── table.jsx # table preview component
+│           └── settings.js # Settings singleton schema
+├── next-sitemap.config.js  # Sitemap configuration for next-sitemap.
+├── next.config.js      # Next.js configuration file.
+├── package-lock.json    # Lock file for npm dependencies.
+├── package.json        # Project metadata and dependencies.
+├── postcss.config.js   # PostCSS configuration.
+├── public            # Public assets (images, fonts, etc.).
+│   ├── fonts       # Font files.
+│   │   ├── Inter-Bold.otf
+│   │   └── Inter-Regular.otf
+│   └── img         # Image files.
+│       ├── opengraph.jpg  # Open Graph default image
+│       └── skeleton.svg    # Skeleton loading animation.
+├── sanity.cli.ts     # Sanity CLI configuration.
+├── sanity.config.ts  # Sanity studio configuration.
+├── styles          # Stylesheets.
+│   ├── prism.css    # Prism CSS for code highlighting.
+│   └── tailwind.css   # Tailwind CSS styles.
+├── tailwind.config.js # Tailwind configuration.
+├── tsconfig.json      # TypeScript configuration.
+└── utils           # Utility functions
+    ├── all.js  # utility functions
+    └── upperFirst.js   # utility functions
+`}
+      </pre>
+      <div className="mt-10 flex justify-center">
+        <Link
+          href="https://github.com/Manish-Tamang/Blog"
+          target="_blank"
+          rel="noopener"
+          className="ring-offset-background focus-visible:ring-ring before:animate-rainbow group relative inline-flex inline-flex h-10 items-center justify-center whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium transition-colors transition-transform duration-200 before:absolute before:bottom-[-20%] before:left-1/2 before:z-[0] before:h-[20%] before:w-[60%] before:-translate-x-1/2 before:bg-[linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] before:[filter:blur(calc(0.8*1rem))] hover:scale-105 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 active:scale-95 disabled:pointer-events-none disabled:opacity-50 dark:bg-[linear-gradient(#121213,#121213),linear-gradient(#121213_50%,rgba(18,18,19,0.6)_80%,rgba(18,18,19,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))]">
+          <div className="flex items-center">
+            <span class="ml-1 p-1 text-black lg:inline dark:text-white">
+              Star on GitHub
+            </span>
+          </div>
+          <div class="ml-2 flex items-center gap-1 text-sm md:flex">
+            <svg
+              class="size-4 text-gray-500 transition-all duration-200 group-hover:text-yellow-300"
+              data-slot="icon"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                clip-rule="evenodd"
+                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                fill-rule="evenodd"></path>
+            </svg>
+            <span class="font-display inline-block font-medium tabular-nums tracking-wider text-black dark:text-white">
+              {stars}
+            </span>
+          </div>
+        </Link>
       </div>
     </Container>
   );
 }
+
+const TechCard = ({ name, version, color, Icon }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <Icon size="3em" color={color} className="mb-2" />
+      <span className="font-medium text-gray-600 dark:text-gray-300">
+        {name}
+      </span>
+
+      {version && (
+        <span className="text-sm text-gray-500 dark:text-gray-500">
+          v{version}
+        </span>
+      )}
+    </div>
+  );
+};
--- app/(website)/contact/contact.js
+++ app/(website)/contact/contact.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 "use client";
 
 import Container from "@/components/container";
@@ -12,6 +14,7 @@ import {
   EnvelopeIcon,
   PhoneIcon
 } from "@heroicons/react/24/outline";
+
 export default function Contact() {
   const [isLoading, setIsLoading] = useState(false);
   const {
@@ -25,7 +28,7 @@ export default function Contact() {
   } = useForm({
     mode: "onTouched"
   });
-  
+
   const {
     data: settings,
     error,
@@ -39,7 +42,8 @@ export default function Contact() {
   const [isSuccess, setIsSuccess] = useState(false);
   const [message, setMessage] = useState(false);
   // Please update the Access Key in the Sanity CMS - Site Congig Page
-  const apiKey = settings?.w3ckey || "cecb189a-5fc5-48fb-b3ab-7b4d5c9da471";
+  const apiKey =
+    settings?.w3ckey || "cecb189a-5fc5-48fb-b3ab-7b4d5c9da471";
 
   const { submit: onSubmit } = useWeb3Forms({
     access_key: apiKey,
@@ -60,40 +64,44 @@ export default function Contact() {
 
   return (
     <Container>
-      <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
+      <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight lg:leading-snug dark:text-white">
         Contact
       </h1>
-      <div className="text-center">
-        <p className="text-lg">Feel This form</p>
-        <p className="text-lg">I'am here to help.</p>
-      </div>
 
-      <div className="grid my-10 md:grid-cols-2">
+      <div className="my-10 grid md:grid-cols-2">
         <div className="my-10">
           <h2 className="text-2xl font-semibold dark:text-white">
             Contact Me
           </h2>
-          <p className="max-w-sm mt-5">
-            Please provide a clear and concise message in the <strong>"Message"</strong> field. Include relevant details about your inquiry to help me understand and address your needs effectively.
+          <p className="mt-5 max-w-sm">
+            Please provide a clear and concise message in the{" "}
+            <strong>"Message"</strong> field. Include relevant details
+            about your inquiry to help me understand and address your
+            needs effectively.
           </p>
-          <h5 className="max-w-sm mt-5"> By submitting this form, you acknowledge that the information you provide will be used to respond to your inquiry. Please review our privacy policy for more information on how we handle your data.</h5>
+          <h5 className="mt-5 max-w-sm">
+            By submitting this form, you acknowledge that the
+            information you provide will be used to respond to your
+            inquiry. Please review our privacy policy for more
+            information on how we handle your data.
+          </h5>
 
           <div className="mt-5">
-            <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-gray-400">
-              <MapPinIcon className="w-4 h-4" />
+            <div className="text-dark-600 mt-2 flex items-center space-x-2 dark:text-gray-400">
+              <MapPinIcon className="h-4 w-4" />
               <span>Budhanilkantha-12, kathmandu, Nepal</span>
             </div>
             {settings?.email && (
-              <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-gray-400">
-                <EnvelopeIcon className="w-4 h-4" />
+              <div className="text-dark-600 mt-2 flex items-center space-x-2 dark:text-gray-400">
+                <EnvelopeIcon className="h-4 w-4" />
                 <a href={`mailto:${settings.email}`}>
                   {settings.email}
                 </a>
               </div>
             )}
             {settings?.phone && (
-              <div className="flex items-center mt-2 space-x-2 text-dark-600 dark:text-gray-400">
-                <PhoneIcon className="w-4 h-4" />
+              <div className="text-dark-600 mt-2 flex items-center space-x-2 dark:text-gray-400">
+                <PhoneIcon className="h-4 w-4" />
                 <a href={`tel:${settings.phone}`}>{settings.phone}</a>
               </div>
             )}
@@ -113,10 +121,11 @@ export default function Contact() {
                 type="text"
                 placeholder="Full Name"
                 autoComplete="false"
-                className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${errors.name
-                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                    : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-                  }`}
+                className={`w-full rounded-md border-2 px-4 py-3 outline-none placeholder:text-gray-800 focus:ring-4 dark:bg-gray-900 dark:text-white   dark:placeholder:text-gray-200  ${
+                  errors.name
+                    ? "border-red-600 ring-red-100 focus:border-red-600 dark:ring-0"
+                    : "border-gray-300 ring-gray-100 focus:border-gray-600 dark:border-gray-600 dark:ring-0 dark:focus:border-white"
+                }`}
                 {...register("name", {
                   required: "Full name is required",
                   maxLength: 80
@@ -139,10 +148,11 @@ export default function Contact() {
                 placeholder="Email Address"
                 name="email"
                 autoComplete="false"
-                className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${errors.email
-                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                    : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-                  }`}
+                className={`w-full rounded-md border-2 px-4 py-3 outline-none placeholder:text-gray-800 focus:ring-4 dark:bg-gray-900 dark:text-white   dark:placeholder:text-gray-200  ${
+                  errors.email
+                    ? "border-red-600 ring-red-100 focus:border-red-600 dark:ring-0"
+                    : "border-gray-300 ring-gray-100 focus:border-gray-600 dark:border-gray-600 dark:ring-0 dark:focus:border-white"
+                }`}
                 {...register("email", {
                   required: "Enter your email",
                   pattern: {
@@ -162,10 +172,11 @@ export default function Contact() {
               <textarea
                 name="message"
                 placeholder="Your Message"
-                className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white dark:placeholder:text-gray-200 dark:bg-gray-900   rounded-md outline-none  h-36 focus:ring-4  ${errors.message
-                    ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                    : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
-                  }`}
+                className={`h-36 w-full rounded-md border-2 px-4 py-3 outline-none placeholder:text-gray-800   focus:ring-4 dark:bg-gray-900  dark:text-white dark:placeholder:text-gray-200  ${
+                  errors.message
+                    ? "border-red-600 ring-red-100 focus:border-red-600 dark:ring-0"
+                    : "border-gray-300 ring-gray-100 focus:border-gray-600 dark:border-gray-600 dark:ring-0 dark:focus:border-white"
+                }`}
                 {...register("message", {
                   required: "Enter your Message"
                 })}
@@ -180,10 +191,10 @@ export default function Contact() {
 
             <button
               type="submit"
-              className="w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-white dark:text-black ">
+              className="w-full rounded-md bg-gray-900 px-7 py-4 font-semibold text-white transition-colors hover:bg-gray-800 focus:outline-none focus:ring focus:ring-gray-200 focus:ring-offset-2 dark:bg-white dark:text-black ">
               {isSubmitting ? (
                 <svg
-                  className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
+                  className="mx-auto h-5 w-5 animate-spin text-white dark:text-black"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24">
@@ -206,12 +217,12 @@ export default function Contact() {
           </form>
 
           {isSubmitSuccessful && isSuccess && (
-            <div className="mt-3 text-sm text-center text-green-500">
+            <div className="mt-3 text-center text-sm text-green-500">
               {message || "Success. Message sent successfully"}
             </div>
           )}
           {isSubmitSuccessful && !isSuccess && (
-            <div className="mt-3 text-sm text-center text-red-500">
+            <div className="mt-3 text-center text-sm text-red-500">
               {message || "Something went wrong. Please try later."}
             </div>
           )}
